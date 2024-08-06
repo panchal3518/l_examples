1c4d2e95e2dd18357cf586e39176ab9d471ca024
panchal3518 <251121@ust.com>
2024-08-06 19:19:22 +0530

    - filter added

--- lib/high_order_functions/filter.dart
+++ lib/high_order_functions/filter.dart
@@ -8,12 +8,46 @@ class FilterFunc extends StatefulWidget {
 }
 
 class _FilterFuncState extends State<FilterFunc> {
+  final _arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Filter'),
+          title: const Text('Filter'),
         ),
-        body: Text(''));
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Array --> $_arr',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Where --> ',
+                  style: Theme.of(context).textTheme.titleSmall,
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text:
+                            '_arr.where((element) => element % 2 == 0).toList()',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: '${whereFunc()}',
+                        style: const TextStyle(fontWeight: FontWeight.normal)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  whereFunc() {
+    final result = _arr.where((element) => element % 2 == 0).toList();
+    return result;
   }
 }
